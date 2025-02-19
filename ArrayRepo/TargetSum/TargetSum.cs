6b52364d99d25888fc7a18867676c491cf97d6b4
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-02-19 08:56:39 +0530

    Add project files.

---
+++ ArrayRepo/TargetSum/TargetSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRepo.TargetSum
+{
+    internal class TargetSum
+    {
+        public int[] arr = new int[] { 2, 7, 11, 15 };
+
+        public int[] Sum(int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int diff = target - arr[i];
+                if (dict.ContainsKey(diff))
+                {
+                    return new int[] { dict[diff], i };
+                }
+                else
+                {
+                    dict.Add(arr[i], i);
+                }
+            }
+            throw new Exception("not found");
+        }
+    }
+}
