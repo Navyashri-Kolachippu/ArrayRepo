7729a1215bbb414da6392f584174120a2e850425
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-02-21 20:35:03 +0530

    Separate array

---
+++ ArrayRepo/3046SeparateArray/Separate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRepo._3046SeparateArray
+{
+    public class Separate
+    {
+        public bool IsPossibleToSplit(int[] nums)
+        {
+            bool result = false;
+            Dictionary<int, int> set1 = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!set1.ContainsKey(nums[i]))
+                {
+                    set1.Add(nums[i], 1);
+                }
+                else
+                {
+                    set1[nums[i]]++;
+                }
+            }
+            result = !set1.Values.Any(x => x >= 3);
+            return result;
+
+        }
+    }
+}
--- ArrayRepo/Program.cs
+++ ArrayRepo/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using ArrayRepo._261Duplicate;
+using ArrayRepo._3046SeparateArray;
 using ArrayRepo.TargetSum;
 
-
+Separate separate=new Separate();
+int[] data = new int[] { 1,1,2, 2, 3, 4 };
+var a = separate.IsPossibleToSplit(data);
